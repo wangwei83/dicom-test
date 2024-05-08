c7e491e97aaf06785e4788ea8684820ad57e6943
wangwei83 <wangwei83@cuit.edu.cn>
2024-05-08 21:55:35 +0800

    保存dicom文件

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from pydicom import dcmread
 from pydicom.data import get_testdata_file
 
 # The path to a pydicom test dataset
-# path = get_testdata_file("CT_small.dcm")
-#ds = dcmread(path)
-path = get_testdata_file("image1.dcm")
-ds = dcmread("image1.dcm")
+path = get_testdata_file("CT_small.dcm")
+ds = dcmread(path)
+
+#ds = dcmread("image1.dcm")
 
 # `arr` is a numpy.ndarray
 arr = ds.pixel_array
